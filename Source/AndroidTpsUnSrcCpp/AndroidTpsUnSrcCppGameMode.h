2d2f7394ea171726c8be055960a761c2e71cf5ba
Vipul Dessai <badgamerbad@gmail.com>
2025-03-14 04:22:07 +0530

    init

---
+++ Source/AndroidTpsUnSrcCpp/AndroidTpsUnSrcCppGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AndroidTpsUnSrcCppGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AAndroidTpsUnSrcCppGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AAndroidTpsUnSrcCppGameMode();
+};
+
+
+
